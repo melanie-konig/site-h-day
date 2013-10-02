a0231c25553363ef0a668e325308b54848cc85b4
Romain MANESCHI <rmaneschi@ITK-P-201202-03.(none)>
2013-10-02 12:02:13 +0200

    put tag search into project page

--- _posts/projets/2013-05-28-xwingandroid.md
+++ _posts/projets/2013-05-28-xwingandroid.md
@@ -4,7 +4,7 @@ nom_unique: xwingandroid
 titre: X-Wing Android
 images: [images/projets/default.png]
 description: "Faire une petite appli native android qui permette de générer des escadrilles X-Wing. On lui dit : on est 5 joueurs, on veut des escadrilles à 100 points et hop, elle propose une escadrille rebelle et une impériale."
-technos: [Android, java]
+technos: [android, java]
 dates: [28/05/2013]
 equipe: [Nelly, Cuong, Laurent]
 categories: [projets]
--- javascripts/filterTag.js
+++ javascripts/filterTag.js
@@ -1,8 +1,21 @@
 $(function () {
-	if(window.location.hash !== undefined && window.location.hash !== "") {
-		var tag = window.location.hash.split("#")[1];
-		$('a[tag='+tag+']').each(function (element, index) {
-			$(element)[0].parentElement.parentElement.hidden = false;
-		});
+	
+	var check = function() {
+		if(window.location.hash !== undefined && window.location.hash !== "") {
+			var tag = window.location.hash.split("#")[1];
+			$('a[tag]').each(function (element, index) {
+				$(element)[0].parentElement.parentElement.hidden = true;
+			});
+			$('a[tag='+tag+']').each(function (element, index) {
+				$(element)[0].parentElement.parentElement.hidden = false;
+			});
+		}
 	}
+
+	check();
+
+	 window.onhashchange = function () {
+    check();
+	}
+
 });
